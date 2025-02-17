d8cdaef50fdc06fce835ec6118e55d4851f02a92
Eshhaa11 <patellesha11@gmail.com>
2025-02-17 17:20:18 +0300

    add services page

---
+++ src/pages/ServicesPage.js
@@ -0,0 +1,16 @@
+import HeaderComponent from "../components/headerComponent"
+
+const ServicePage = () => {
+    return (
+        <div>
+            <HeaderComponent/>
+            <h1>Services</h1>
+            
+
+
+            
+        </div>
+    )
+}
+
+export default ServicePage
