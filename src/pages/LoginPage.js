38d523e3377b6474cf4c75da0a803a1e3dbc2ef2
Eshhaa11 <patellesha11@gmail.com>
2025-02-17 13:53:05 +0300

    Bootstrap template

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import HeaderComponent from "../components/headerComponent"
+
+const LoginPage = () => {
+    return (
+        <div>
+            <HeaderComponent/>
+            <h1>Login</h1>
+        </div>
+    )
+}
+
+export default LoginPage;
---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,12 @@
+import HeaderComponent from "../components/headerComponent"
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <HeaderComponent/>
+            <h1>Register</h1>
+        </div>
+    )
+}
+
+export default RegisterPage
