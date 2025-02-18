799068b11d832d9cc302e0addad9e4586f98258b
Eshhaa11 <patellesha11@gmail.com>
2025-02-18 11:48:32 +0300

    add

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,15 +1,43 @@
 import HeaderComponent from "../components/headerComponent"
+import './RegistrationForm.css';
 
 const RegisterPage = () => {
     return (
         <div>
             <HeaderComponent/>
             <h1>Register</h1>
-            
-
-
-            
-        </div>
+            <form>
+  <div class="form-group">
+    <label>Full Name</label>
+    <input type="text" class="form-control" placeholder="Enter full name"/>
+  </div>
+  <div class="form-group">
+    <label>Phone Number</label>
+    <input type="tel" class="form-control" placeholder="Enter phone number"/>
+  </div>
+  <div class="form-group">
+    <label>Email address</label>
+    <input type="email" class="form-control" placeholder="Enter email"/>
+  </div>
+  <div class="form-group">
+    <label>Password</label>
+    <input type="password" class="form-control" placeholder="Password"/>
+  </div>
+  <div class="form-group">
+    <label>Confirm Password</label>
+    <input type="password" class="form-control" placeholder="Confirm password"/>
+  </div>
+  <div class="form-group">
+    <label>Gender</label>
+    <select id="cars">
+      <option value="female">Female</option>
+      <option value="male">Male</option>
+      <option value="other">Other</option>
+    </select>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+     </form>
+     </div>
     )
 }
 
