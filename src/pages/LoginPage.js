7c53e76d8e3fba261d130f863e578f3e1ab9e98b
Natasha-n-j <natashanjkaranja@gmail.com>
2025-02-18 16:43:14 +0300

    added login form

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,6 +5,12 @@ const LoginPage = () => {
         <div>
             <HeaderComponent/>
             <h1>Login</h1>
+            <form>
+              <input type="email" name="email" placeholder="Email" required/>
+              <input type="tel" name="phone" placeholder="Phone Number" required/>
+              <input type="password" name="password" placeholder="Password" required/>
+              <button type="submit">Login</button>
+           </form>
         </div>
     )
 }
