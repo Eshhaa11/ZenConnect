00fcaf24751ef8ba07123928e4a22f784ba4327c
Eshhaa11 <patellesha11@gmail.com>
2025-02-20 08:16:04 +0300

    refactor registration form layout and styling for improved user experience

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,66 +1,68 @@
-import HeaderComponent from "../components/headerComponent"
-import './RegistrationForm.css';
+import HeaderComponent from "../components/headerComponent";
+import "./RegistrationForm.css";
 
 const RegisterPage = () => {
-    return (
-        <div class = "container">
-            <HeaderComponent/>
-           <div class="title">Registration Form</div>
-            <form action = "#">
-              <div class="user-details">
-                <div class = "input-box">
-                  <span class="details">Full Name</span>
-                  <input type="text" placeholder="Enter your full name" required/>
-                </div>
-                <div class = "input-box">
-                  <span class="details">User Name</span>
-                  <input type="text" placeholder="Enter your user name" required/>
-                </div>
-                <div class = "input-box">
-                  <span class="details">Phone Number</span>
-                  <input type="number" placeholder="Enter your phone number" required/>
-                </div>
-                <div class = "input-box">
-                  <span class="details">Email</span>
-                  <input type="email" placeholder="Enter your email" required/>
-                </div>
-                <div class = "input-box">
-                  <span class="details">Password</span>
-                  <input type="password" placeholder="Enter your password" required/>
-                </div>
-                <div class = "input-box">
-                  <span class="details">Password</span>
-                  <input type="password" placeholder="Enter your password once more" required/>
-                </div>
-                <div class = "gender-details">
-                  <input type ="radio" name="gender" id="dot-1"/>
-                  <input type ="radio" name="gender" id="dot-2"/>
-                  <input type ="radio" name="gender" id="dot-3"/>
-
-                  <span class="gender-title">Gender</span>
-                  <div class="category">
-                    <label for ="dot-1">
-                      <span class= "dot one"></span>
-                      <span class= "gender">Female</span>
-                      </label>
-                      <label for ="dot-2">
-                      <span class= "dot two"></span>
-                      <span class= "gender">Male</span>
-                      </label>
-                      <label for ="dot-3">
-                      <span class= "dot three"></span>
-                      <span class= "gender">Other</span>
-                      </label>
-                </div>
+  return (
+    <div>
+      <HeaderComponent />
+      <div className="container">
+        <div className="title">Registration</div>
+        <div className="content">
+          <form action="#">
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Full Name</span>
+                <input type="text" placeholder="Enter your name" required />
               </div>
-              <div class = "button">
-                <input type="submit" value="Register"/>
+              <div className="input-box">
+                <span className="details">Username</span>
+                <input type="text" placeholder="Enter your username" required />
+              </div>
+              <div className="input-box">
+                <span className="details">Email</span>
+                <input type="text" placeholder="Enter your email" required />
+              </div>
+              <div className="input-box">
+                <span className="details">Phone Number</span>
+                <input type="text" placeholder="Enter your number" required />
+              </div>
+              <div className="input-box">
+                <span className="details">Password</span>
+                <input type="password" placeholder="Enter your password" required />
+              </div>
+              <div className="input-box">
+                <span className="details">Confirm Password</span>
+                <input type="password" placeholder="Confirm your password" required />
               </div>
             </div>
-  
-            </form>
+            <div className="gender-details">
+              <span className="gender-title">Gender</span>
+              <div className="category">
+                <label htmlFor="dot-1">
+                  <input type="radio" name="gender" id="dot-1" />
+                  <span className="dot one"></span>
+                  Male
+                </label>
+                <label htmlFor="dot-2">
+                  <input type="radio" name="gender" id="dot-2" />
+                  <span className="dot two"></span>
+                  Female
+                </label>
+                <label htmlFor="dot-3">
+                  <input type="radio" name="gender" id="dot-3" />
+                  <span className="dot three"></span>
+                  Prefer not to say
+                </label>
+              </div>
+            </div>
+            <div className="button">
+              <input type="submit" value="Register" />
+            </div>
+          </form>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
